22a96d1ec0ac88b1449b78b75d27e45578d37dbe
Gajus Kuizinas <g.kuizinas@anuary.com>
2014-01-08 14:38:21 +0000

    error_message now must be an array, defining both, the error message naming the input and the version without input name. the former is used to display error message summary, while the latter is used to display the error below the input itself.

--- translator.class.php
+++ translator.class.php
@@ -19,13 +19,16 @@ class Translator {
 				// Give 'vladfoo' selector 'Vlad Foo' name.
 				'vladfoo' => 'Vlad Foo'
 			],
-			'rule_error' => [
+			'validator_error' => [
 				// Replace the default email invalid_format error message.
-				'ay\vlad\rule\email.invalid_format' => '{vlad.subject.name} must be a valid email address.'
+				'ay\vlad\validator\email.invalid_format' => [
+					'{vlad.subject.name} must be a valid email address.',
+					'The input must be a valid email address.'
+				]
 			],
-			'rule_error_selector' => [
+			'validator_error_selector' => [
 				// Replace the default email invalid_format error message for a specific selector.
-				'ay\vlad\rule\email.invalid_format vladfoo' => 'Oops. Email address does not seem to be valid.'
+				'ay\vlad\validator\email.invalid_format vladfoo' => 'Oops. Email address does not seem to be valid.'
 			]
 		];
 
@@ -40,14 +43,34 @@ class Translator {
 	 * @return void
 	 */
 	final private function populate (array $dictionary) {
-		if (array_diff(array_keys($dictionary), ['selector', 'rule_error', 'rule_error_selector'])) {
-			throw new \BadMethodCallException('$dictionary must be an array containing at least one sub-array defining "selector", "rule_error" or "rule_error_selector".');
+		if (array_diff(array_keys($dictionary), ['selector', 'validator_error', 'validator_error_selector'])) {
+			throw new \BadMethodCallException('$dictionary must be an array containing at least one sub-array defining "selector", "validator_error" or "validator_error_selector".');
 		}
 
-		foreach ($dictionary as $type => $translations) {
-			foreach ($translations as $t) {
-				if (!is_string($t)) {
-					throw new \BadMethodCallException('Individual translations must be a string.');
+		if (isset($dictionary['selector'])) {
+			foreach ($dictionary['selector'] as $translation) {
+				if (!is_string($translation)) {
+					throw new \BadMethodCallException('Selector translation must be a string.');
+				}
+			}
+		}
+
+		if (isset($dictionary['validator_error_selector'])) {
+			foreach ($dictionary['validator_error_selector'] as $translation) {
+				if (!is_string($translation)) {
+					throw new \BadMethodCallException('Selector specific validation translation must be a string.');
+				}
+			}
+		}
+
+		if (isset($dictionary['validator_error'])) {
+			foreach ($dictionary['validator_error'] as $translation) {
+				if (!is_array($translation)) {
+					throw new \Exception('Error message must be an array.');
+				} else if (count($translation) != 2) {
+					throw new \Exception('Error message array must be exactly two messages long.');
+				} else if (!isset($translation[0], $translation[1]) || !is_string($translation[0]) || !is_string($translation[1])) {
+					throw new \Exception('Invalid error message format.');
 				}
 			}
 		}
@@ -67,41 +90,59 @@ class Translator {
 	}
 
 	/**
-	 * Retrieve error message from the $dictionary or use the default Rule error message.
+	 * Retrieve error message from the $dictionary or use the default Validator error message.
 	 * Replace variables in the resulting error message.
 	 *
 	 * @param Error $error
-	 * @param Rule $rule
+	 * @param Validator $validator
 	 * @param Subject $subject
 	 * @return string
 	 */
-	public function getErrorMessage ($error_name, Rule $rule, Subject $subject) {
-		$rule_error = mb_strtolower( get_class($rule) ) . '.' . $error_name;
-		$rule_error_selector = $rule_error . ' ' . $subject->getSelector();
+	public function getErrorMessage ($error_name, Validator $validator, Subject $subject) {
+		$validator_error = mb_strtolower( get_class($validator) ) . '.' . $error_name;
+		$validator_error_selector = $validator_error . ' ' . $subject->getSelector();
 
-		// Enforce check presense of the error message in the original Rule.
-		$message = $rule->getMessage($error_name);
+		// Enforce check presense of the error message in the original Validator.
+		$message = $validator->getMessage($error_name);
 
-		if (isset($this->dictionary['rule_error_selector'][$rule_error_selector])) {
-			$message = $this->dictionary['rule_error_selector'][$rule_error_selector];
-		} else if (isset($this->dictionary['rule_error'][$rule_error])) {
-			$message = $this->dictionary['rule_error'][$rule_error];
+		if (isset($this->dictionary['validator_error_selector'][$validator_error_selector])) {
+			$message = $this->dictionary['validator_error_selector'][$validator_error_selector];
+			
+			if (!is_string($message)) {
+				throw new \Exception('Selector specific error message must be a string.');
+			
+			}
+			// This is input specific error already.
+			$message = [$message, null];
+		} else if (isset($this->dictionary['validator_error'][$validator_error])) {
+			$message = $this->dictionary['validator_error'][$validator_error];
 		}
 
-		$message = preg_replace_callback('/\{vlad\.([a-z\.]+)}/i', function ($e) use ($subject, $rule) { // \.(?:a-z\.)+\}
+		if (!is_array($message)) {
+			throw new \Exception('Error message must be an array.');
+		} else if (count($message) != 2) {
+			throw new \Exception('Error message array must be exactly two messages long.');
+		} else if (!isset($message[0], $message[1]) || !is_string($message[0]) || !is_string($message[1])) {
+			// validator_error_selector is allowed to omit the generic message.
+			if (!isset($this->dictionary['validator_error_selector'][$validator_error_selector]) || !is_null($message[1])) {
+				throw new \Exception('Invalid error message format.');
+			}			
+		}
+
+		$message = preg_replace_callback('/\{vlad\.([a-z\.]+)}/i', function ($e) use ($subject, $validator) { // \.(?:a-z\.)+\}
 			$path = explode('.', $e[1]);
 
 			if ($path[0] === 'subject' && $path[1] === 'name') {
 				return $subject->getName();
-			} else if ($path[0] === 'rule' && $path[1] === 'options') {
-				$options = $rule->getOptions();
+			} else if ($path[0] === 'validator' && $path[1] === 'options') {
+				$options = $validator->getOptions();
 
 				if (isset($path[2]) && isset($options[$path[2]]) && is_scalar($options[$path[2]])) {
 					return $options[$path[2]];
 				}
 			}
 
-			throw new Exception('Unknown variable ' . $e[0] . '.');
+			throw new \Exception('Unknown variable ' . $e[0] . '.');
 		}, $message);
 
 		return $message;
