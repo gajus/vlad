d82b3d1c58a7a4a57456ff6c44c8f2b3c4c41fb7
Gajus Kuizinas <g.kuizinas@anuary.com>
2014-01-20 19:07:05 +0000

    Added Email syntax test.

---
+++ tests/ValidatorEmailTest.php
@@ -0,0 +1,35 @@
+<?php
+class ValidatorEmailTest extends PHPUnit_Framework_TestCase {
+	public function testValidSyntax () {
+		$test = new \gajus\vlad\Test();
+		$test->assert('foo', 'email');
+
+		$assessment = $test->assess(['foo' => 'vlad@test.com']);
+
+		$this->assertCount(0, $assessment);
+	}
+
+	/**
+	 * Since the underlying implementation is using PHP filter_var, there
+	 * is no reason to extensively test different (in)valid email addressses.
+	 */
+	public function testInvalidSyntax () {
+		$test = new \gajus\vlad\Test();
+		$test->assert('foo', 'email');
+
+		$assessment = $test->assess(['foo' => 'vlad']);
+
+		$this->assertCount(1, $assessment);
+		$this->assertSame('invalid_syntax', $assessment[0]->getName());
+	}
+
+	/**
+	 * @expectedException InvalidArgumentException
+	 */
+	public function testNotScalarInput () {
+		$test = new \gajus\vlad\Test();
+		$test->assert('foo', 'email');
+
+		$assessment = $test->assess(['foo' => null]);
+	}
+}
