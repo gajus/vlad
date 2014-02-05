040ab494a94e7349695e231b679f3316157ae251
Gajus Kuizinas <g.kuizinas@anuary.com>
2014-02-05 16:13:12 +0000

    Clean up.

--- src/gajus/vlad/validator/credit_card/pan.php
+++ src/gajus/vlad/validator/credit_card/pan.php
@@ -16,64 +16,8 @@ class Pan extends \gajus\vlad\Validator {
 			'invalid_checksum' => [
 				'{vlad.subject.name} is not a valid credit card number.',
 				'The input is not a valid credit card number.'
-			],
-			/*'unsupported_card' => [
-				'{vlad.subject.name} is not a supported card type.',
-				'The input is not a supported card type.'
-			]*/
-		];/*,
-		$default_options = [
-			'type' => ['visa', 'mastercard', 'amex', 'maestro']
-		],*/
-		/**
-		 * @see Visa, MasterCard, American Express (http://www.regular-expressions.info/creditcard.html), Maestro (http://regexlib.com/REDetails.aspx?regexp_id=1626)
-		 */
-		/*$card_pattern = [
-			'visa' => '/^4[0-9]{12}(?:[0-9]{3})?$/',
-			'mastercard' => '/^5[1-5][0-9]{14}$/',
-			'amex' => '/^3[47][0-9]{13}$/',
-			'maestro' => '/
-				(^(5[0678])\d{11,18}$) |
-				(^(6[^0357])\d{11,18}$) |
-				(^(601)[^1]\d{9,16}$) |
-				(^(6011)\d{9,11}$) |
-				(^(6011)\d{13,16}$) |
-				(^(65)\d{11,13}$) |
-				(^(65)\d{15,18}$) |
-				(^(633)[^34](\d{9,16}$)) |
-				(^(6333)[0-4](\d{8,10}$)) |
-				(^(6333)[0-4](\d{12}$)) |
-				(^(6333)[0-4](\d{15}$)) |
-				(^(6333)[5-9](\d{8,10}$)) |
-				(^(6333)[5-9](\d{12}$)) |
-				(^(6333)[5-9](\d{15}$)) |
-				(^(6334)[0-4](\d{8,10}$)) |
-				(^(6334)[0-4](\d{12}$)) |
-				(^(6334)[0-4](\d{15}$)) |
-				(^(67)[^(59)](\d{9,16}$)) |
-				(^(6759)](\d{9,11}$)) |
-				(^(6759)](\d{13}$)) |
-				(^(6759)](\d{16}$)) |
-				(^(67)[^(67)](\d{9,16}$)) |
-				(^(6767)](\d{9,11}$)) |
-				(^(6767)](\d{13}$)) |
-				(^(6767)](\d{16}$))
-			/'
-		];*/
-	
-	/*public function __construct (array $options = []) {
-		parent::__construct($options);
-
-		$options = $this->getOptions();
-
-		if (empty($options['type'])) {
-			throw new \InvalidArgumentException('Type property is empty.');
-		} else if (!is_array($options['type'])) {
-			throw new \InvalidArgumentException('Type property is not array.');
-		} else if (array_diff($options['type'], self::$default_options['type'])) {
-			throw new \InvalidArgumentException('Type property includes an unsupported card type.');
-		}
-	}*/
+			]
+		];
 
 	protected function validate (\gajus\vlad\Subject $subject) {
 		$value = $subject->getValue();
@@ -86,20 +30,6 @@ class Pan extends \gajus\vlad\Validator {
 		if (!$this->isValidChecksum($value)) {
 			return 'invalid_checksum';
 		}
-
-		/*$card_type = null;
-
-		foreach (static::$card_pattern as $type => $pattern) {
-			if (preg_match($pattern, $value)) {
-				$card_type = $type;
-
-				break;
-			}
-		}
-
-		if (!$card_type || !in_array($card_type, $options['type'])) {
-			return 'unsupported_card';
-		}*/
 	}
 
 	/**
