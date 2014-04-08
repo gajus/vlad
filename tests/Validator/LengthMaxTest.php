461f59160cdc9b9107c282350385dfb75c260b60
Gajus Kuizinas <g.kuizinas@anuary.com>
2014-04-08 03:12:56 +0000

    Added LengthMax validator.

---
+++ tests/Validator/LengthMaxTest.php
@@ -0,0 +1,26 @@
+<?php
+class LengthMaxTest extends PHPUnit_Framework_TestCase {
+    /**
+     * @expectedException Gajus\Vlad\Exception\InvalidArgumentException
+     * @expectedExceptionMessage "length" option must be a whole number.
+     */
+    public function testInvalidMinParameter () {
+        new \Gajus\Vlad\Validator\LengthMax(['length' => 'test']);
+    }
+
+    public function testTooLong () {
+        $validator = new \Gajus\Vlad\Validator\LengthMax(['length' => 2]);
+        
+        $assessment = $validator->assess('bar');
+
+        $this->assertFalse($assessment);
+    }
+
+    public function testNotTooLong () {
+        $validator = new \Gajus\Vlad\Validator\LengthMax(['length' => 3]);
+        
+        $assessment = $validator->assess('bar');
+
+        $this->assertTrue($assessment);
+    }
+}
